186c849e658d8698f2b3757bca49233ef573c3d4
enmanuelneri <enmanuel.uscuvilca@unmsm.edu.pe>
2019-08-19 08:05:33 -0500

    2da modificacion

--- app/src/main/java/com/enul/recyclerview_demoo/MainActivity.java
+++ app/src/main/java/com/enul/recyclerview_demoo/MainActivity.java
@@ -2,12 +2,22 @@ package com.enul.recyclerview_demoo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*Creamos una instacia de la clase "RecyclerView" y otra de nuestro "Adaptador" creado anteriormente*/
+    private RecyclerView recyclerViewCasa;
+    private RecyclerViewAdaptador adaptadorCasa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* OJO: A continuacion vinculamos nuestra insstancia  recyclerViewCasa con el recyclerView de
+         * nuestro Layout */
+
+        recyclerViewCasa=(RecyclerView)findViewById(R.id.recyclerCasa);
+
     }
 }
